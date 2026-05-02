3a46d4a5adf3d7c83ad25a21d9745ef21405c5fa
alik.kuttybayev <alik.kuttybayev@gmail.com>
2026-05-03 01:04:52 +0500

    Update assignment

--- src/main/java/HashTableTest.java
+++ src/main/java/HashTableTest.java
@@ -1,2 +1,19 @@
-public class HashTableTest  {
+import java.util.Random;
+
+public class HashTableTest {
+    public static void main(String[] args) {
+        Random random = new Random();
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>(101);
+        for (int i = 0; i < 10000; i++) {
+            int id = i;
+            String name = "Name" + random.nextInt(100000);
+
+            MyTestingClass key = new MyTestingClass(id, name);
+            Student student = new Student(i, "Student" + i, 2.0 + random.nextDouble() * 2.0);
+
+            table.put(key, student);
+        }
+        System.out.println("Total elements: " + table.size());
+        table.printBucketSizes();
+    }
 }
--- src/main/java/MyTestingClass.java
+++ src/main/java/MyTestingClass.java
@@ -1,2 +1,70 @@
 public class MyTestingClass {
+    private int id;
+    private String name;
+
+    public MyTestingClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 17;
+
+        hash = 31 * hash + id;
+
+        if (name != null) {
+            for (int i = 0; i < name.length(); i++) {
+                hash = 31 * hash + name.charAt(i);
+            }
+        }
+
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof MyTestingClass)) {
+            return false;
+        }
+
+        MyTestingClass other = (MyTestingClass) obj;
+
+        if (this.id != other.id) {
+            return false;
+        }
+
+        if (this.name == null && other.name != null) {
+            return false;
+        }
+
+        if (this.name != null && !this.name.equals(other.name)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MyTestingClass{id=" + id + ", name='" + name + "'}";
+    }
 }
+
+
+
+
+
+

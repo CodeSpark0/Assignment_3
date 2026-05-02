309e27142e1c03e46bf22dc0c9720253f482e437
alik.kuttybayev <alik.kuttybayev@gmail.com>
2026-05-03 03:57:15 +0500

    Update assignment

--- src/main/java/BSTTest.java
+++ src/main/java/BSTTest.java
@@ -1,36 +1,39 @@
 public class BSTTest {
     public static void main(String[] args) {
+
+        // Create a hard coded BST.
+        //
+        //             20
+        //            /  \
+        //           8    22
+        //          / \
+        //         4   12
+        //            /  \
+        //           10   14
+
         BST<Integer, String> tree = new BST<>();
 
-        // 1. Add elements
+        tree.put(20, "twenty");
         tree.put(8, "eight");
-        tree.put(3, "three");
-        tree.put(10, "ten");
-        tree.put(1, "one");
-        tree.put(6, "six");
-        tree.put(14, "fourteen");
+        tree.put(22, "twenty two");
         tree.put(4, "four");
-        tree.put(7, "seven");
-        tree.put(13, "thirteen");
+        tree.put(12, "twelve");
+        tree.put(10, "ten");
+        tree.put(14, "fourteen");
 
-        // 2. Check size
         System.out.println("Size: " + tree.size());
 
-        // 3. Check get()
-        System.out.println("Get key 6: " + tree.get(6));
-        System.out.println("Get key 10: " + tree.get(10));
+        System.out.println("Get key 12: " + tree.get(12));
         System.out.println("Get key 100: " + tree.get(100));
 
-        // 4. Check in-order iteration
-        System.out.println("\nTree elements in-order:");
+        System.out.println("\nIn-order traversal:");
         for (var elem : tree) {
             System.out.println("key is " + elem.getKey() + " and value is " + elem.getValue());
         }
 
-        // 5. Check delete()
-        tree.delete(10);
+        tree.delete(8);
 
-        System.out.println("\nAfter deleting key 10:");
+        System.out.println("\nAfter deleting key 8:");
         System.out.println("Size: " + tree.size());
 
         for (var elem : tree) {
